efb87dd15e31033145f7e2d494796c4996db0bf9
markarmenia <160868447+markarmenia@users.noreply.github.com>
2024-07-12 19:22:06 +0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,2 @@
-import rdkit
-from rdkit import Chem
-# from rdkit.Chem import Draw
-
-
-def substructure_search(mols: list, mol: str) -> list:
-    substructure = Chem.MolFromSmiles(mol)
-    matches = []
-    for smiles in mols:
-        chem_mol = Chem.MolFromSmiles(smiles)
-        match = chem_mol.HasSubstructMatch(substructure)
-        if match:
-            matches.append(smiles)
-    print(matches)
-
-substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1ccccc1")
+def substructure_search(mols, mol):
+    pass
