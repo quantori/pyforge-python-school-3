2474ee334cdb084d4c4a078377542128ac989c04
Gaioz <tabtadzegaga@gmail.com>
2024-07-12 16:57:34 +0400

    implement src/main.py substructure_search method using HOF map and filter

--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+
+
 def substructure_search(mols, mol):
-    pass
+    molecule = Chem.MolFromSmiles(mol)
+    return list(filter(lambda x: x.HasSubstructMatch(molecule), map(lambda x: Chem.MolFromSmiles(x), mols)))
+
+
+mols = ["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"]
+mol = "c1ccccc1"
+matches = substructure_search(mols, mol)
+img = Draw.MolsToGridImage(matches)
+img.show()
