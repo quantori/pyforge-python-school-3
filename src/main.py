c579e027c75b730680a31aaff61c71e15fb90520
nshan_yegyan23 <nshan_yegyan23@alumni.aua.am>
2024-07-12 19:20:29 +0400

    hw1

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
-def substructure_search(mols, mol):
-    pass
+import rdkit
+from rdkit import Chem
+# from rdkit.Chem import Draw
+
+
+def substructure_search(mols: list, mol: str) -> list:
+    substructure = Chem.MolFromSmiles(mol)
+    matches = []
+    for smiles in mols:
+        chem_mol = Chem.MolFromSmiles(smiles)
+        match = chem_mol.HasSubstructMatch(substructure)
+        if match:
+            matches.append(smiles)
+    print(matches)
+
+substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1ccccc1")
