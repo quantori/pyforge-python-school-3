7138c4ffe89d55e25b9685504b4daa4c2dcc8224
SabaXdev <sabakatamadze@gmail.com>
2024-07-14 10:50:25 +0400

    Final commit, implement substructure_search function

--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
+from rdkit import Chem
+
+
 def substructure_search(mols, mol):
-    pass
+    # Convert the SMILES string to an RDKit molecule object
+    molecule = Chem.MolFromSmiles(mol)
+    return [smile for smile in mols if Chem.MolFromSmiles(smile).HasSubstructMatch(molecule)]
