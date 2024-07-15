ac32e23c3c9f6251e90b69c7e88ccc2cfc27c8b6
natatapkina <135203029+natatapkina@users.noreply.github.com>
2024-07-15 18:52:07 +0700

    Implement substructure search function

--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
-def substructure_search(mols, mol):
-    pass
+from rdkit import Chem
+
+
+def substructure_search(structures_smiles: list[str], substructure_smiles: str) -> list[str]: 
+    search_result = []
+    substructure_mol = Chem.MolFromSmiles(substructure_smiles)
+
+    for structure_smiles in structures_smiles:
+        structure_mol = Chem.MolFromSmiles(structure_smiles)
+
+        if structure_mol.HasSubstructMatch(substructure_mol):
+            search_result.append(structure_smiles)
+
+    return search_result
