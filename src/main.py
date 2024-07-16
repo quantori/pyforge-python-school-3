816f55513e86917dc16f8239620df3c49fb563c8
maneav78 <avoyan.mane@mail.ru>
2024-07-16 16:32:59 +0400

    created substructure_search function

--- src/main.py
+++ src/main.py
@@ -1,2 +1,29 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+
 def substructure_search(mols, mol):
-    pass
+    substructure = Chem.MolFromSmiles(mol)
+
+    if substructure is None:
+        raise ValueError(f'Invalid substructure SMILES: {mol}')
+    
+    substructure_num_atoms = substructure.GetNumAtoms()
+
+    matches = []
+    for molecule in mols:
+        object_mol = Chem.MolFromSmiles(molecule)
+        if object_mol is None:
+            print(f'Skipping invalid molecule SMILES: {molecule}')
+            continue
+
+        if object_mol.GetNumAtoms() < substructure_num_atoms:
+            continue 
+        
+        if object_mol.HasSubstructMatch(substructure):
+           matches.append(molecule)
+
+    return matches
+
+# testing
+if __name__ == "__main__":
+    print(substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1ccccc1"))
