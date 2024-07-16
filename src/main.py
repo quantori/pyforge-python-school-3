344ad371a18a4598c71f6b4b644b2bd53faa9fa7
ggagua <giorgi.gagua.1@btu.edu.ge>
2024-07-16 08:18:00 +0400

    Homework-1: Substructure Search

--- src/main.py
+++ src/main.py
@@ -1,2 +1,19 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+
 def substructure_search(mols, mol):
-    pass
+    substructure = Chem.MolFromSmiles(mol)
+
+    matches = []
+
+    for mol in mols:
+        molecule = Chem.MolFromSmiles(mol)
+
+        if molecule.HasSubstructMatch(substructure):
+            matches.append(mol)
+
+    return matches
+
+# Perform a substructure search
+final = substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1ccccc1")
+print(final)
