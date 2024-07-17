066fd873f2623e0abc69fc01ffa4ab07a29c6167
M.J <janelidze.marita@kiu.edu.ge>
2024-07-18 00:08:45 +0400

    update substructure_search implementation

--- src/main.py
+++ src/main.py
@@ -7,29 +7,12 @@ def substructure_search(mols, mol):
     substructure = Chem.MolFromSmiles(mol)
 
     # List to store molecules that contain the substructure
-    matching_molecules = []
-    matching_molecule_structures = []
-
-    # Iterate through the list of molecule SMILES
-    for smiles in mols:
-        # Convert the SMILES to an RDKit molecule
-        molecule = Chem.MolFromSmiles(smiles)
-
-        # Check if the molecule contains the substructure
-        if molecule.HasSubstructMatch(substructure):
-            # If it matches, add the SMILES to the matching molecules list
-            matching_molecules.append(smiles)
-            matching_molecule_structures.append(molecule)
+    matching_molecules = [smiles for smiles in mols if Chem.MolFromSmiles(smiles).HasSubstructMatch(substructure)]
 
     # Visualize the matching molecules
-    if matching_molecule_structures:
-        img = Draw.MolsToGridImage(matching_molecule_structures, molsPerRow=2, subImgSize=(500, 500), returnPNG=False)
+    if matching_molecules:
+        matching_structures = [Chem.MolFromSmiles(smiles) for smiles in matching_molecules]
+        img = Draw.MolsToGridImage(matching_structures, molsPerRow=2, subImgSize=(500, 500), returnPNG=False)
         img.show()
 
     return matching_molecules
-
-
-"""
-#Example:
-substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1ccccc1")
-"""
