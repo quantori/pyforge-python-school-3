ec91044951508d1de29fbf237dac86e7e26d7855
Giorgi <giorgi.iliaevi@gmail.com>
2024-07-18 11:58:31 +0400

    Homework 4

--- src/main.py
+++ src/main.py
@@ -1,2 +1,17 @@
+from rdkit import Chem
+
+
 def substructure_search(mols, mol):
-    pass
+    matching_molecules = []
+    try:
+        substructure_mol = Chem.MolFromSmiles(mol)
+
+        for smiles in mols:
+            molecule = Chem.MolFromSmiles(smiles)
+
+            if molecule.HasSubstructMatch(substructure_mol):
+                matching_molecules.append(smiles)
+    except Exception as e:
+        print(e)
+
+    return matching_molecules
