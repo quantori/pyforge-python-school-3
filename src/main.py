bcec173bc6f50ff0a86a2ee058706b0187e64eaa
Deko17 <kenessov.daryn@gmail.com>
2024-07-18 15:42:00 +0500

    homework

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
-def substructure_search(mols, mol):
-    pass
+from rdkit import Chem
+
+def substructure_search(smiles_list, sub_smiles):
+    sub_mol = Chem.MolFromSmiles(sub_smiles) #for conversion substructure SMILES to RDKIT molecujle
+    matches = []        #empty list for storage of molecules
+    for smiles in smiles_list:  #converts each smilel to rdkit mol and if it is created it checks if it is valid structure
+        mol = Chem.MolFromSmiles(smiles)
+        if mol and mol.HasSubstructMatch(sub_mol):      
+            #adds molecules that are matched to list
+            matches.append(smiles)
+    return matches
+
+molecules = ["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"]
+substructure = "c1ccccc1"
+result = substructure_search(molecules, substructure)
+print(result) 
