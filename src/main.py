f3aa4a2300c7ca7adc5eca072d7d0e8399620440
unknown <komronis01@gmail.com>
2024-07-19 17:33:28 +0500

    first try
    Trying to upload to the github, done the function

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
+from rdkit import Chem
+
 def substructure_search(mols, mol):
-    pass
+    
+    substructure = Chem.MolFromSmiles(mol) #convering into smiles
+    matched_molecules = [] #storing matches here
+    for smiles in mols: #iterating through the given list
+        molecule = Chem.MolFromSmiles(smiles)
+        if molecule.HasSubstructMatch(substructure): 
+            matched_molecules.append(smiles)
+    return matched_molecules
+
+
