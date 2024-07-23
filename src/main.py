975fe57897bc6b2cc99d482faea52e0d97cd4caa
Albert Amadanyan <105919321+a1brt@users.noreply.github.com>
2024-07-23 10:37:01 +0000

    fix: molecules typo in main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.routers import moleculs
+from src.routers import molecules
 
 app = FastAPI(title="Python Summer School 2024: SMILES")
-app.include_router(moleculs.router, prefix="/molecules", tags=["molecules"])
+app.include_router(molecules.router, prefix="/molecules", tags=["molecules"])
