fc9efbdaa69e352c714b19e7c32ad0a517a3acb1
HideOnShroud <natroshviliguga5@gmail.com>
2024-07-24 23:37:47 +0400

    added add molecule, edit molecule

--- hw/2/src/main.py
+++ hw/2/src/main.py
@@ -28,3 +28,23 @@ def retrieve_molecule(identifier: str):
             return molecule
     raise HTTPException(status_code=404, detail="Molecule not found")
 
+
+@app.post("/add")
+def add_molecules(molecule: Molecules):
+    # I don't know if smiles all need to be uppercase so I will leave it as it is.
+    for mol in molecules_db:
+        if mol.identifier == molecule.identifier:
+            raise HTTPException(status_code=400, detail="Molecule with this identifier already exists")
+    molecules_db.append(molecule)
+    return molecules_db
+
+
+@app.put("/smiles/{identifier}")
+def update_molecule(identifier:str, updated_smile: Molecules):
+    for index, molecule in enumerate(molecules_db):
+        if molecule.identifier == identifier:
+            molecules_db[index] = updated_smile
+            return updated_smile
+    raise HTTPException(status_code=404, detail="Molecule not found")
+
+
