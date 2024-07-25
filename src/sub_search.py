2fa157157fadafc9a25c5a3e5816f678a1c0c27a
qua_nyakhina <qua_nyakhina@exactsciences.com>
2024-07-25 21:26:47 +0200

    task about API

---
+++ src/sub_search.py
@@ -0,0 +1,30 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+
+def visualize(molecules: list) -> None:
+    # Draw the molecules in a grid
+    img = Draw.MolsToGridImage(molecules, molsPerRow=2, subImgSize=(200, 200), returnPNG=False)
+    img.show()
+
+def substructure_search(mols: list, mol: str) -> list:
+    """takes two arguments: 1) List of molecules as SMILES strings 2) Substructure as SMILES string
+    Function returns a list of all molecules from argument 1 that contain substructure from argument 2
+    """
+
+    # Create a molecules from strings
+    smile_mol = Chem.MolFromSmiles(mol)
+    molecules = [Chem.MolFromSmiles(smiles) for smiles in mols]
+
+    match_list = []
+    for element in molecules:
+        # Perform the substructure search
+        if element.HasSubstructMatch(smile_mol):
+            match_list.append(element)
+
+    visualize(match_list)
+
+    return match_list
+
+
+substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1ccccc1")
+substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "CCO")
