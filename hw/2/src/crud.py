3912fd21eef804e0569784cc57232f8fbee292d8
HideOnShroud <natroshviliguga5@gmail.com>
2024-09-02 16:10:30 +0400

    pagination and logger

--- hw/2/src/crud.py
+++ hw/2/src/crud.py
@@ -2,54 +2,80 @@ from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from models import Molecule
 from schemas import MoleculeSchema
+from logger import logger
 import io
 import csv
 
 
-def get_molecules(db: Session):
-    return db.query(Molecule).all()
+def get_molecules(db: Session, limit: int = None, offset: int = 0):
+    logger.info("Fetching all molecules from the database")
+    
+    if limit:
+        query = db.query(Molecule).offset(offset).limit(limit)
+    else:
+        query = db.query(Molecule)
+        
+    for molecule in query:
+        logger.debug(f"Found molecule with identifier {molecule.identifier}")
+        yield molecule
+    return query
 
 def get_molecule_by_id(db: Session, identifier: str):
+    logger.info(f"Fetching molecule with identifier {identifier} from the database")
     query = db.query(Molecule).filter(Molecule.identifier == identifier).first()
     if query:
+        logger.debug(f"Found molecule with identifier {identifier}")
         return query
+    logger.warning(f"Molecule with identifier {identifier} not found")
     return None
 
 def update_molecule(db: Session, identifier: str, updated_molecule: MoleculeSchema):
+    logger.info(f"Updating molecule with identifier {identifier}")
     molecule = db.query(Molecule).filter(Molecule.identifier == identifier).first()
     
     if not molecule:
+        logger.warning(f"Molecule with identifier {identifier} not found")
         return None
     
     if updated_molecule.identifier != identifier:
         existing_molecule = db.query(Molecule).filter(Molecule.identifier == updated_molecule.identifier).first()
         if existing_molecule:
+            logger.error(f"Molecule with identifier {updated_molecule.identifier} already exists")
             raise ValueError("A molecule with the new identifier already exists.")
     
     molecule.identifier = updated_molecule.identifier
     molecule.smile = updated_molecule.smile
     db.commit()
+    logger.debug(f"Molecule with identifier {identifier} updated successfully")
     return molecule
 
 def delete_molecule(db: Session, identifier: str):
+    logger.info(f"Deleting molecule with identifier {identifier}")
     molecule = db.query(Molecule).filter(Molecule.identifier == identifier).first()
     if molecule:
         db.delete(molecule)
         db.commit()
+        logger
         return {"detail": "Molecule deleted"}
+    logger.warning(f"Molecule with identifier {identifier} not found")
     return None
 
 def add_molecule(db: Session, molecule: MoleculeSchema):
+    logger.info(f"Adding molecule with identifier {molecule.identifier}")
     new_molecule = Molecule(identifier=molecule.identifier, smile=molecule.smile)
     if new_molecule:
         db.add(new_molecule)
         db.commit()
+        logger.debug(f"Molecule with identifier {molecule.identifier} added successfully")
         return new_molecule
+    logger.error(f"Failed to add molecule with identifier {molecule.identifier}")
     return None
 
 def add_molecule_from_csv(db: Session, csv_file):
+    logger.info("Adding molecules from CSV file")
     # Ensure the file has a .csv extension
     if not csv_file.filename.endswith('.csv'):
+        logger.error("Only CSV files are supported")
         raise HTTPException(status_code=400, detail="Only CSV files are supported")
 
     # Read the file content
@@ -60,6 +86,7 @@ def add_molecule_from_csv(db: Session, csv_file):
         # Parse the CSV data
         csv_reader = csv.DictReader(data, delimiter='\t')
     except Exception as e:
+        logger.error(f"Failed to read CSV: {str(e)}")
         raise HTTPException(status_code=400, detail=f"Failed to read CSV: {str(e)}")
 
     added_smiles = []
@@ -69,11 +96,13 @@ def add_molecule_from_csv(db: Session, csv_file):
 
         # Check for missing data
         if not identifier or not smile:
+            logger.error("File must contain 'identifier' and 'smile' columns")  
             raise HTTPException(status_code=400, detail="File must contain 'identifier' and 'smile' columns")
 
         # Check if the molecule already exists
         existing_molecule = db.query(Molecule).filter(Molecule.identifier == identifier).first()
         if not existing_molecule:
+            logger.debug(f"Adding molecule with identifier {identifier}")
             new_molecule = Molecule(identifier=identifier, smile=smile)
             added_smiles.append(new_molecule)
             db.add(new_molecule)
@@ -83,6 +112,7 @@ def add_molecule_from_csv(db: Session, csv_file):
 
     # Return None if no molecules were added
     if not added_smiles:
+        logger.warning("No molecules were added")
         return None
 
     # Return the added molecules
